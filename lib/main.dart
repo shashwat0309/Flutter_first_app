ee7e32c0415315a3a1fe77c07dbd7cbdaa4906b8
shashwat0309 <shashwat.mohite@gmail.com>
2021-12-02 19:02:38 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/homepage.dart';
 import 'pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,10 @@ class MyApp extends StatelessWidget {
     String name = "shashwat";
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,  
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
